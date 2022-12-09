a3e1ceb3c8e8afd9916fdc6d9022bab110d0e92a
sameer-ahmed123 <sameerkhanjan123@gmail.com>
2022-12-10 00:23:42 +0500

    changes to main page icons

--- messaging/urls.py
+++ messaging/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from messaging.views import Message_template
+from messaging.views import Message_template,Directs
 
 urlpatterns = [
     path("DM_page/",Message_template, name="Message_template"),
+    path("directs/<username>/", Directs, name="directs"),
 ]
--- messaging/views.py
+++ messaging/views.py
@@ -29,3 +29,22 @@ def Message_template(request):
         "active_user": active_user
     }
     return render(request, "DMS/Message.html", context)
+
+@login_required
+def Directs(request, username):
+    user = request.user
+    messages = Message.get_messages(user=user)
+    active_direct = username
+    directs = Message.objects.filter(user=user, recipent=username)
+    directs.update(is_read=True)
+
+    for message in messages:
+        if message['user'].username == username:
+            message['unread'] = 0
+
+    context = {
+        "directs":directs,
+        "messages": messages,
+        "active_direct":active_direct,
+    }
+    return render(request, "DMS/Message.html", context)
